9cb81de02319adfa62192bf401f5bb528d890367
KateShan <kitushenoy@gmail.com>
2017-04-04 12:10:10 -0700

    code to tail only few lines

---
+++ tail.py
@@ -0,0 +1,40 @@
+# tail only number of lines and returns 
+import os,sys
+def tail(fname, window):
+	#open(fname, 'rb')
+        """Read last N lines from file fname."""
+        if window <= 0:
+            raise ValueError('invalid window value %r' % window)
+        with open(fname) as f:
+            BUFSIZ = 1024
+            # True if open() was overridden and file was opened in text
+            # mode. In that case readlines() will return unicode strings
+            # instead of bytes.
+            encoded = getattr(f, 'encoding', False)
+            CR = '\n' if encoded else b'\n'
+            data = '' if encoded else b''
+            f.seek(0, os.SEEK_END)
+            fsize = f.tell()
+            block = -1
+            exit = False
+            while not exit:
+                step = (block * BUFSIZ)
+                if abs(step) >= fsize:
+                    f.seek(0)
+                    newdata = f.read(BUFSIZ - (abs(step) - fsize))
+                    exit = True
+                else:
+                    f.seek(step, os.SEEK_END)
+                    newdata = f.read(BUFSIZ)
+                data = newdata + data
+                if data.count(CR) >= window:
+                    break
+                else:
+                    block -= 1
+            return data.splitlines()[-window:]
+
+
+
+line = tail("test.py",10)
+for n in line:
+	print n
